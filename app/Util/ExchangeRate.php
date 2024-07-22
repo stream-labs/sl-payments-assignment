81f7a7b2e0bc7d4b01125bca6e001a5320ec609d
Daniel Puente <dpuente@logitech.com>
2024-07-22 16:34:16 -0500

    er and updated seed

---
+++ app/Util/ExchangeRate.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Util;
+
+class ExchangeRate
+{
+    public static float $USD_TO_USD = 1;
+    public static float $GBP_TO_USD = 1.3;
+    public static float $EUR_TO_USD = 1.08;
+}
